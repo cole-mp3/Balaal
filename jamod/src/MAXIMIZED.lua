1bd48533dedada40531fed96958bc7a79f6520d7
Jabong <99981800+cole-mp3@users.noreply.github.com>
2025-10-20 09:58:40 -0400

    Added the first maximized joker ( i know for a fact some shit went wrong)

--- jamod/src/MAXIMIZED.lua
+++ jamod/src/MAXIMIZED.lua
@@ -9,3 +9,49 @@ SMODS.ObjectType ({
 	end,
   }
   )
+  SMODS.Rarity {
+    key = "Max",
+    pools = {
+        ["Joker"] = true
+    },
+    default_weight = 0.01,
+    badge_colour = HEX('00008B'),
+    loc_txt = {
+        name = "MAXIMIZED"
+    },
+    get_weight = function(self, weight, object_type)
+        return weight
+    end,
+}
+SMODS.Joker {
+  key = "everyman"
+  --this card's getting its own atlas just gimme a sec
+  atlas = "Jatlas",
+    pos = { x = 1, y = 0 },
+    blueprint_compat = true,
+     cost = 4,
+    discovered = true,
+    rarity = "jabong_Max",
+    loc_txt = {
+      name = "EVERYMAN"
+      text = {
+        "Applies an {C:attention}All Seal{} to"
+        "All played cards. Retriggers cards with any seal."
+        "Also turns all cards Negative."
+      }
+    }
+    config = { extra = {repetitions = 1}}
+    loc_vars = function(self, info_queue, card)
+      return {}
+    end,
+    calculate = function(self, card, context)
+    if context.individual and context.cardarea == G.play then
+            context.other_card:set_seal("jabong_canofall",true,true)
+            context.other_card:set_edition("e_negative",true,true)              
+    end
+    if context.repetition and context.cardarea == G.play then
+     repetitions = card.ability.extra.repetitions    
+    end
+
+  end,
+}
--- jamod/src/seals.lua
+++ jamod/src/seals.lua
@@ -4,6 +4,12 @@ SMODS.Atlas ({
     px = 142,
     py = 150
 })
+SMODS.Atlas ({
+    key = "alls",
+    path = "Seels/allseal.png",
+    px = 71,
+    py = 95
+})
 SMODS.Seal {
     key = 'stone_seal',
      badge_colour = HEX("1d4fd7"),
@@ -22,6 +28,32 @@ SMODS.Seal {
             return {}
     end,
 
+
+}
+SMODS.Seal {
+    key = 'canofall'
+    badge_colour = HEX("8B0000")
+    config = {extra = {repetitions = 2, chips = 50, xmult = 1.5, mult = 20, xchips = 2.5,  }},
+    loc_vars = function(self, info_queue)
+        return { vars = { self.config.extra.chips, self.config.extra.xmult, self.config.extra.mult
+    self.config.extra.xmult, self.config.extra.xchips, self.config.extra.repetitions } }
+    end,
+    atlas = "alls",
+    pos = {x=0, y=0},
+    calculate = function(self, card, context)
+            if context.main_scoring and context.cardarea == G.play or if context.repetition and context.cardarea then
+                return {
+                    chips = self.config.extra.chips
+                    mult = self.config.extra.mult
+                    xmult = self.config.extra.xmult
+                    xchips = self.config.extra.xchips
+                }
+                if context.repetition then
+                    repetitions = card.ability.extra.repetitions
+                end
+            end
+            return {}
+    end,
 
 }
 
