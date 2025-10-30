9e6a5f94950899172f80525b0bb453e8e8d2a56b
Jabong <99981800+cole-mp3@users.noreply.github.com>
2025-10-30 10:56:03 -0400

    a

--- jamod/src/MAXIMIZED.lua
+++ jamod/src/MAXIMIZED.lua
@@ -1,4 +1,21 @@
 -- alias i will fucking kill you
+
+--the below is commented cuz im not sure if it works or not an i refuse to gamble
+--[[
+SMODS.Sound ({
+    vol = 0.6,
+    pitch = 0.7,
+    key = "womp",
+    path = "music_vigilante.ogg",
+    if G.jokers then
+        if next(SMODS.find_card("j_jabong_gundx")) then
+            select_music_track(self) 
+                return (150)
+            end
+        end
+    end
+})
+    ]]
 SMODS.Rarity {
     key = "Max",
     pools = {
