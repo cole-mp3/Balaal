369a59372127e535997d0e7af7e8d8dc4169b624
Jabong <99981800+cole-mp3@users.noreply.github.com>
2025-10-31 12:42:47 -0400

    booster pack

--- jamod/src/basiccs.lua
+++ jamod/src/basiccs.lua
@@ -411,6 +411,70 @@ SMODS.Consumable {
         return G.jokers and #G.jokers.cards < G.jokers.config.card_limit
     end,
 }
+SMODS.Booster {
+    key = "matbooster",
+    atlas = "Jonklers/jatlas.png",
+    loc_txt = {
+        name = "Material Pack",
+        text = {
+            "Choose one of up to 3 {C:attetnion}Material{} cards."
+        },
+        group_name = "Material_Pack"
+    },
+    group_key = "k_Material_Pack"
+    config = {extra = 3, choose = 1}
+    loc_vars = function(self, info_queue, card)
+        local cfg = (card and card.ability) or self.config
+        return {
+            vars = { cfg.choose, cfg.extra },
+            key = self.key:sub(1, -3), -- This uses the description key of the booster without the number at the end
+        }
+    end,
+    ease_background_colour = function(self)
+        ease_background_colour_blind(G.STATES.TAROT_PACK)
+    end,
+    particles = function(self)
+        G.booster_pack_sparkles = Particles(1, 1, 0, 0, {
+            timer = 0.015,
+            scale = 0.2,
+            initialize = true,
+            lifespan = 1,
+            speed = 1.1,
+            padding = -1,
+            attach = G.ROOM_ATTACH,
+            colours = { G.C.WHITE, lighten(G.C.PURPLE, 0.4), lighten(G.C.PURPLE, 0.2), lighten(G.C.GOLD, 0.2) },
+            fill = true
+        })
+        G.booster_pack_sparkles.fade_alpha = 1
+        G.booster_pack_sparkles:fade(1, 0)
+    end,
+    create_card = function(self, card, i)
+        local _card
+        if G.GAME.used_vouchers.v_omen_globe and pseudorandom('omen_globe') > 0.8 then
+            _card = {
+                set = "Spectral",
+                area = G.pack_cards,
+                skip_materialize = true,
+                soulable = true,
+                key_append =
+                "vremade_ar2"
+            }
+        else
+            _card = {
+                set = "jabong_Material",
+                area = G.pack_cards,
+                skip_materialize = true,
+                soulable = true,
+                key_append =
+                "vremade_ar2"
+            }
+        end
+        return _card
+    end,
+}
+
+
+}
  
            
 
