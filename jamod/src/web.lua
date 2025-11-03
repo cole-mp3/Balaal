d26ec0c647f4bd29105463b065cc0d24a0e9427e
Jabong <99981800+cole-mp3@users.noreply.github.com>
2025-11-03 13:55:28 -0500

    added luchador tag

--- jamod/src/web.lua
+++ jamod/src/web.lua
@@ -5,3 +5,31 @@ SMODS.Atlas {
     px = 142,
     py = 190
 }
+SMODS.Atlas {
+    key = "tatlas",
+    path = "tatlas.png",
+    px = 32,
+    py = 32
+}
+SMODS.Tag {
+    key = "luchalibre",
+    atlas = "tatlas"
+    pos = { x = 0, y = 0 },
+    loc_txt = {
+        name = "Luchador Tag",
+        text = {
+            "Creates {C:attention}Luchador{}.",
+            "{C:inactive}(Must have room){}"
+        }
+    }
+    apply = function(self, tag, context)
+         if context.type == 'new_blind_choice' then
+            SMODS.add_card {
+            key = "j_luchador",                       
+                         }
+            tag.triggered = true
+            return true
+        end
+    end
+}
+
